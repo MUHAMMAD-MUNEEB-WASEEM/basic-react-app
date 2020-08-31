ac3fcdcf4998c8de86d0ca66e0e983283a1b3e04
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2020-08-31 15:58:57 +0500

    Creating basic React app

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from "./Hello";//importing Hello from Hello.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      
-        <p>Made by Muhammad Muneeb</p>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(props)//just random variable in order to use properties
+//We can also write like this-> App({name, age})
+//so instead of using props.name or props.age, we will simply
+//use {name} and {age} where needed 
+
+{
+  return <div>
+          Hello from {props.name} and age is {props.age - 5}
+          <Hello fname={props.name} age={props.age}></Hello>
+        </div>//this is not js, it is jsx->javascript xml
+           
 }
+//In above, hello function will works like a tag and we pass it name and age, 
+//but keep in mind that name and age should also be use as parameter in app, like 
+//if in index.js, we donot pass name and age and we had already call it here in hello tag,
+//then it will raise an error
 
-export default App;
+export default App;//export app function to be use in index.js using <App />, or <div><App /></div>
